59641982efed6924130ec2383de078f63b3bb629
ipercin <ipercin44@gmail.com>
2017-06-11 05:31:35 +0300

    Databindings
    databinding with two way fetch input data.

--- AngularFirst/src/app/app.module.ts
+++ AngularFirst/src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 //Yeni componenti import edip dosya konumunu ekliyoruz
 import { ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component'
+import { ServersComponent } from './servers/servers.component'
+
 
 //Yeni eklenen komponentleri declare ediyoruz
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularFirst/src/app/servers/servers.component.ts
+++ AngularFirst/src/app/servers/servers.component.ts
@@ -7,17 +7,28 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //selector:'[app-servers]',
   //selector: '.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created! ';
+  serverName= 'Testserver';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created ! Server name is : '+ this.serverName;
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
